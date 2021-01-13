2083dde9968fa7396566e5c6f38ef664976e22f7
chledowski <jakub.chledowski@gmail.com>
2021-01-13 12:44:16 +0100

    Readme, update scripts

--- run_training.sh
+++ run_training.sh
@@ -6,14 +6,14 @@ FRACTION=$3
 DAGGER=$4
 RESULT_DIR=$5
 
-#if [ "${FRACTION}" != "100" ]; then
-#    NUM_LINES=$(wc -l "cache_replacement/policy_learning/cache/traces/${DATASET}_train.csv")
+if [ "${FRACTION}" != "100" ]; then
+    NUM_LINES=$(wc -l "cache_replacement/policy_learning/cache/traces/${DATASET}_train.csv")
 #    var=$(echo "scale=2;${NUM_LINES}*${FRACTION}" |bc)
 #    echo $var
 #    echo $NUM_LINES
 #    echo $FRACTION
-#
-#fi
+
+fi
 
 #if [ "${DAGGER}" == "True" ]; then
 #
