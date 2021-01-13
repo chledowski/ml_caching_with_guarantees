c0e984c4d35da513259a70bd26d49397eee546f6
chledowski <jakub.chledowski@gmail.com>
2021-01-13 12:38:28 +0100

    Readme, update scripts

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # ML caching with guarantees
 
+## Introduction
+The cache replacement pipeline has been cloned and slightly differed from https://github.com/google-research/google-research/tree/master/cache_replacement.
 
+## Getting the traces
+To avoid problems with reproduction, we have saved all the traces used in our work to [Dropbox](https://www.dropbox.com/sh/h9lsrxgmofl2oso/AABgLHNNmla2X2ipqTGiL9Oua?dl=0). To move forward, please copy the traces you would like to work
+ on to the `./cache_replacement/policy_learning/cache/traces/` folder.
+ 
+## Setting up the environment
+This project uses `python3`. Please install the required packages with
+  
+  `pip install -r requirements.txt`
+   
+Afterwards, please install OpenAI baselines with 
+ 
+ ```pip install -e git+https://github.com/openai/baselines.git@ea25b9e8b234e6ee1bca43083f8f3cf974143998#egg=baselines```
+ 
+## Training models
+To run training, use this command:
+
+```./run_training.sh <DATASET> <DEVICE> <FRACTION> <DAGGER> <RESULT_DIR>```
+
+where:
+- DATASET - the name of used dataset (do not forget to download all three splits: train, valid, test)
+- DEVICE - the GPU device to train on
+- FRACTION - the fratcion of the train set to use (in \% from 1 to 100)
+- DAGGER (`True` or `False`) - whether to use DAgger
+- RESULT_DIR - data folder
+
+For example:
+
+```./run_training.sh astar 0 33 True ./results```
+
+## Evaluation of the trained models
--- run_training.sh
+++ run_training.sh
@@ -4,33 +4,42 @@ DATASET=$1
 DEVICE=$2
 FRACTION=$3
 DAGGER=$4
+RESULT_DIR=$5
 
-
-if [ "${DAGGER}" == "True" ]; then
-
-    CUDA_VISIBLE_DEVICES=${DEVICE} python3 -m cache_replacement.policy_learning.cache_model.main \
-        --experiment_base_dir="${DATA}/oa" \
-        --experiment_name="${DATASET}__dagger=true__fraction=${FRACTION}" \
-        --cache_configs="cache_replacement/policy_learning/cache/configs/default.json" \
-        --model_bindings="loss=[\"ndcg\", \"reuse_dist\"]" \
-        --dagger_schedule_bindings="update_freq=5000" \
-        --train_memtrace="cache_replacement/policy_learning/cache/traces/${DATASET}_train.csv" \
-        --valid_memtrace="cache_replacement/policy_learning/cache/traces/${DATASET}_valid.csv" \
-        --total_steps=1500000
-
-else
-
-    CUDA_VISIBLE_DEVICES=${DEVICE} python3 -m cache_replacement.policy_learning.cache_model.main \
-        --experiment_base_dir="${DATA}/oa" \
-        --experiment_name="${DATASET}__dagger=false__fraction=${FRACTION}" \
-        --cache_configs="cache_replacement/policy_learning/cache/configs/default.json" \
-        --model_bindings="loss=[\"ndcg\", \"reuse_dist\"]" \
-        --dagger_schedule_bindings="update_freq=100000000000" \
-        --dagger_schedule_bindings="initial=0" \
-        --dagger_schedule_bindings="final=0" \
-        --dagger_schedule_bindings="num_steps=1" \
-        --train_memtrace="cache_replacement/policy_learning/cache/traces/${DATASET}_train.csv" \
-        --valid_memtrace="cache_replacement/policy_learning/cache/traces/${DATASET}_valid.csv" \
-        --total_steps=1500000
+if [ "${FRACTION}" != "100" ]; then
+    NUM_LINES=$(wc -l "cache_replacement/policy_learning/cache/traces/${DATASET}_train.csv")
+    var=$(echo "scale=2;${NUM_LINES}*${FRACTION}" |bc)
+    echo $var
+    echo $NUM_LINES
+    echo $FRACTION
 
 fi
+
+#if [ "${DAGGER}" == "True" ]; then
+#
+#    CUDA_VISIBLE_DEVICES=${DEVICE} python3 -m cache_replacement.policy_learning.cache_model.main \
+#        --experiment_base_dir="${RESULT_DIR}" \
+#        --experiment_name="${DATASET}__dagger=true__fraction=${FRACTION}" \
+#        --cache_configs="cache_replacement/policy_learning/cache/configs/default.json" \
+#        --model_bindings="loss=[\"ndcg\", \"reuse_dist\"]" \
+#        --dagger_schedule_bindings="update_freq=5000" \
+#        --train_memtrace="cache_replacement/policy_learning/cache/traces/${DATASET}_train.csv" \
+#        --valid_memtrace="cache_replacement/policy_learning/cache/traces/${DATASET}_valid.csv" \
+#        --total_steps=1500000
+#
+#else
+#
+#    CUDA_VISIBLE_DEVICES=${DEVICE} python3 -m cache_replacement.policy_learning.cache_model.main \
+#        --experiment_base_dir="${RESULT_DIR}" \
+#        --experiment_name="${DATASET}__dagger=false__fraction=${FRACTION}" \
+#        --cache_configs="cache_replacement/policy_learning/cache/configs/default.json" \
+#        --model_bindings="loss=[\"ndcg\", \"reuse_dist\"]" \
+#        --dagger_schedule_bindings="update_freq=100000000000" \
+#        --dagger_schedule_bindings="initial=0" \
+#        --dagger_schedule_bindings="final=0" \
+#        --dagger_schedule_bindings="num_steps=1" \
+#        --train_memtrace="cache_replacement/policy_learning/cache/traces/${DATASET}_train.csv" \
+#        --valid_memtrace="cache_replacement/policy_learning/cache/traces/${DATASET}_valid.csv" \
+#        --total_steps=1500000
+#
+#fi
