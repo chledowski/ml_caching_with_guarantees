302cc3c2cc6f4f33f44925af888b820b38d5409c
chledowski <jakub.chledowski@gmail.com>
2021-01-21 02:19:58 +0100

    Parsing and logging

--- run.sh
+++ run.sh
@@ -11,15 +11,15 @@ SAVE_EVAL_FREQ=$7
 # Train on fraction
 if [ "${FRACTION}" != "1" ]; then
 
-#    WC=($(wc -l "cache_replacement/policy_learning/cache/traces/${DATASET}_train.csv"))
-#    NUM_LINES=${WC[0]}
-#    echo "${NUM_LINES}"
-#    DECREASED_NUM_LINES=$(echo "scale=4; $NUM_LINES*$FRACTION" | bc)
-#    echo "${DECREASED_NUM_LINES}"
-#    DECREASED_NUM_LINES_INT=${DECREASED_NUM_LINES%.*}
-#    echo "${DECREASED_NUM_LINES_INT}"
-#    head -n "${DECREASED_NUM_LINES_INT}" "cache_replacement/policy_learning/cache/traces/${DATASET}_train.csv" \
-#     > "cache_replacement/policy_learning/cache/traces/${DATASET}_train_${FRACTION}.csv"
+    WC=($(wc -l "cache_replacement/policy_learning/cache/traces/${DATASET}_train.csv"))
+    NUM_LINES=${WC[0]}
+    echo "${NUM_LINES}"
+    DECREASED_NUM_LINES=$(echo "scale=4; $NUM_LINES*$FRACTION" | bc)
+    echo "${DECREASED_NUM_LINES}"
+    DECREASED_NUM_LINES_INT=${DECREASED_NUM_LINES%.*}
+    echo "${DECREASED_NUM_LINES_INT}"
+    head -n "${DECREASED_NUM_LINES_INT}" "cache_replacement/policy_learning/cache/traces/${DATASET}_train.csv" \
+     > "cache_replacement/policy_learning/cache/traces/${DATASET}_train_${FRACTION}.csv"
 
     TRAIN_TRACE="cache_replacement/policy_learning/cache/traces/${DATASET}_train_${FRACTION}.csv"
 
@@ -30,36 +30,36 @@ fi
 if [ "${DAGGER}" == "True" ]; then
     EXP_NAME="${DATASET}__dagger=true__fraction=${FRACTION}"
 
-#    CUDA_VISIBLE_DEVICES=${DEVICE} python3 -m cache_replacement.policy_learning.cache_model.main \
-#        --experiment_base_dir="${RESULT_DIR}" \
-#        --experiment_name="${EXP_NAME}" \
-#        --cache_configs="cache_replacement/policy_learning/cache/configs/default.json" \
-#        --model_bindings="loss=[\"ndcg\", \"reuse_dist\"]" \
-#        --model_bindings="address_embedder.max_vocab_size=5000" \
-#        --dagger_schedule_bindings="update_freq=5000" \
-#        --train_memtrace="${TRAIN_TRACE}" \
-#        --valid_memtrace="cache_replacement/policy_learning/cache/traces/${DATASET}_valid.csv" \
-#        --total_steps="${STEPS}" \
-#        --save_freq="${SAVE_EVAL_FREQ}" \
-#        --full_eval_freq="${SAVE_EVAL_FREQ}"
+    CUDA_VISIBLE_DEVICES=${DEVICE} python3 -m cache_replacement.policy_learning.cache_model.main \
+        --experiment_base_dir="${RESULT_DIR}" \
+        --experiment_name="${EXP_NAME}" \
+        --cache_configs="cache_replacement/policy_learning/cache/configs/default.json" \
+        --model_bindings="loss=[\"ndcg\", \"reuse_dist\"]" \
+        --model_bindings="address_embedder.max_vocab_size=5000" \
+        --dagger_schedule_bindings="update_freq=5000" \
+        --train_memtrace="${TRAIN_TRACE}" \
+        --valid_memtrace="cache_replacement/policy_learning/cache/traces/${DATASET}_valid.csv" \
+        --total_steps="${STEPS}" \
+        --save_freq="${SAVE_EVAL_FREQ}" \
+        --full_eval_freq="${SAVE_EVAL_FREQ}"
 
 else
     EXP_NAME="${DATASET}__dagger=false__fraction=${FRACTION}"
-#    CUDA_VISIBLE_DEVICES=${DEVICE} python3 -m cache_replacement.policy_learning.cache_model.main \
-#        --experiment_base_dir="${RESULT_DIR}" \
-#        --experiment_name="${EXP_NAME}" \
-#        --cache_configs="cache_replacement/policy_learning/cache/configs/default.json" \
-#        --model_bindings="loss=[\"ndcg\", \"reuse_dist\"]" \
-#        --model_bindings="address_embedder.max_vocab_size=5000" \
-#        --dagger_schedule_bindings="update_freq=100000000000" \
-#        --dagger_schedule_bindings="initial=0" \
-#        --dagger_schedule_bindings="final=0" \
-#        --dagger_schedule_bindings="num_steps=1" \
-#        --train_memtrace="${TRAIN_TRACE}" \
-#        --valid_memtrace="cache_replacement/policy_learning/cache/traces/${DATASET}_valid.csv" \
-#        --total_steps="${STEPS}" \
-#        --save_freq="${SAVE_EVAL_FREQ}" \
-#        --full_eval_freq="${SAVE_EVAL_FREQ}"
+    CUDA_VISIBLE_DEVICES=${DEVICE} python3 -m cache_replacement.policy_learning.cache_model.main \
+        --experiment_base_dir="${RESULT_DIR}" \
+        --experiment_name="${EXP_NAME}" \
+        --cache_configs="cache_replacement/policy_learning/cache/configs/default.json" \
+        --model_bindings="loss=[\"ndcg\", \"reuse_dist\"]" \
+        --model_bindings="address_embedder.max_vocab_size=5000" \
+        --dagger_schedule_bindings="update_freq=100000000000" \
+        --dagger_schedule_bindings="initial=0" \
+        --dagger_schedule_bindings="final=0" \
+        --dagger_schedule_bindings="num_steps=1" \
+        --train_memtrace="${TRAIN_TRACE}" \
+        --valid_memtrace="cache_replacement/policy_learning/cache/traces/${DATASET}_valid.csv" \
+        --total_steps="${STEPS}" \
+        --save_freq="${SAVE_EVAL_FREQ}" \
+        --full_eval_freq="${SAVE_EVAL_FREQ}"
 
 fi
 
